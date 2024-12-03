c591a568f6498f20219838cb2294e3c6f5b58574
chidera-777 <ferdinandchidera49@gmail.com>
2024-12-03 21:47:38 +0100

    removed the init function

--- chatbot.py
+++ chatbot.py
@@ -128,5 +128,4 @@ def main():
                 asyncio.run(display_new_message(st.session_state.all_messages[-1], len(st.session_state.all_messages)-1))
                 
 if __name__ == "__main__":
-    init()
     main()
